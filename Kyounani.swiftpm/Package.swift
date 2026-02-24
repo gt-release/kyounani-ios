1a894a49a043d2ecc32513f54e813e324b50f308
gt-release <51015980+gt-release@users.noreply.github.com>
2026-02-24 12:17:45 +0900

    Fix Playgrounds app target manifest parsing error

--- Kyounani.swiftpm/Package.swift
+++ Kyounani.swiftpm/Package.swift
@@ -1,6 +1,52 @@
 // swift-tools-version: 5.9
 import PackageDescription
 
+#if os(iOS)
+let package = Package(
+    name: "KyounaniPlaygrounds",
+    platforms: [
+        .iOS(.v17)
+    ],
+    products: [
+        .iOSApplication(
+            name: "きょうなに？",
+            targets: ["KyounaniPlaygrounds"],
+            bundleIdentifier: "dev.kyounani.playgrounds",
+            teamIdentifier: "",
+            displayVersion: "1.0",
+            bundleVersion: "1",
+            appIcon: .placeholder(icon: .calendar),
+            accentColor: .presetColor(.blue),
+            supportedDeviceFamilies: [
+                .pad
+            ],
+            supportedInterfaceOrientations: [
+                .portrait,
+                .landscapeLeft,
+                .landscapeRight
+            ]
+        )
+    ],
+    targets: [
+        .target(
+            name: "KyounaniApp",
+            path: "Packages/KyounaniEmbeddedApp/Sources/KyounaniApp",
+            resources: [
+                .process("Resources")
+            ]
+        ),
+        .executableTarget(
+            name: "KyounaniPlaygrounds",
+            dependencies: [
+                "KyounaniApp"
+            ],
+            resources: [
+                .process("Resources")
+            ]
+        )
+    ]
+)
+#else
 let package = Package(
     name: "KyounaniPlaygrounds",
     platforms: [
@@ -25,3 +71,4 @@ let package = Package(
         )
     ]
 )
+#endif
